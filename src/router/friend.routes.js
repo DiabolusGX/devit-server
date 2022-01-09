e221a93120ea6a4109a37a802173b18d1193dea8
Ankit Malik <ankit.malik1092@gmail.com>
2022-01-09 20:19:40 +0530

    show friend requests routes

--- src/router/friend.routes.js
+++ src/router/friend.routes.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.get("/suggestions", isLoggedIn, friendController.suggestions);
 router.post("/add/:userID", isLoggedIn, friendController.addFriend);
 router.post("/remove/:userID", isLoggedIn, friendController.removeFriend);
+router.get("/requests/received", isLoggedIn, friendController.getIncomingReq);
+router.get("/requests/sent", isLoggedIn, friendController.getOutgoingReq);
 router.post("/request/accept/:reqID", isLoggedIn, friendController.acceptReq);
 router.post("/request/cancel/:reqID", isLoggedIn, friendController.cancelReq);
 router.post("/request/reject/:reqID", isLoggedIn, friendController.rejectReq);
